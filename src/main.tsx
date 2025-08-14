b09bbd08bc4ac2fe3026d4e13e69a54e4f280ec2
yjin <devyzyn@gmail.com>
2025-08-14 17:59:14 +0900

    chore: implement react-router-dom for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import Router from './router'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/*' element={<Router />} />)
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
