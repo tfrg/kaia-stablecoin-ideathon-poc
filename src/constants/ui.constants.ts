aa5a317b31a3701d0a1eb695fe9074840ce087cb
yjin <devyzyn@gmail.com>
2025-08-19 23:20:49 +0900

    feat(ui): dark/light mode

---
+++ src/constants/ui.constants.ts
@@ -0,0 +1,2 @@
+// Storage Key
+export const UI_THEME_STORAGE_KEY = 'kaia-stablecoin-ideathon-poc-ui-theme'
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import {
 } from 'react-router-dom'
 import './index.css'
 import Router from './router'
+import { ThemeProvider } from './context/theme-context'
+
+import { UI_THEME_STORAGE_KEY } from './constants/ui.constants'
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path='/*' element={<Router />} />)
@@ -15,6 +18,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme='dark' storageKey={UI_THEME_STORAGE_KEY}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 )
