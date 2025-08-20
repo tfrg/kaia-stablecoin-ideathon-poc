191391d87d0c2f084973ea15abd39d1055afb3ef
yjin <devyzyn@gmail.com>
2025-08-20 17:44:27 +0900

    feat: Add Loader component

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,3 +1,10 @@
+import { LoaderIcon } from 'lucide-react'
+
 export default function Loader() {
-  return <span>loader</span>
+  return (
+    <div className='w-screen h-svh flex  justify-center items-center'>
+      <LoaderIcon className='animate-spin' size={32} />
+      <span className='sr-only'>loading</span>
+    </div>
+  )
 }
